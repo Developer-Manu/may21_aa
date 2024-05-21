af8c830a9f8ba4a0601646de5733422184f7a72d
Manohar Reddy <manoharreddychiluka1516@gmail.com>
2024-05-21 18:05:50 -0500

    first class added

--- src/main/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplication.java
+++ src/main/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoProjectOnGitApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		FirstClass obj1 = new FirstClass();
+		obj1.method1();
 	}
 
 }
---
+++ src/main/java/practice/on/may1/Demo/project/on/git/FirstClass.java
@@ -0,0 +1,7 @@
+package practice.on.may1.Demo.project.on.git;
+
+public class FirstClass {
+    public void method1(){
+        System.out.println("First class method1");
+    }
+}
