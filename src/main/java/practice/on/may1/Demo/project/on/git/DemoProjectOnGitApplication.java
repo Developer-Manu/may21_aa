89afcee97b1a1677127056243c07b1fed50f6153
Manohar Reddy <manoharreddychiluka1516@gmail.com>
2024-05-21 17:27:54 -0500

    first commit

--- src/main/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplication.java
+++ src/main/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoProjectOnGitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoProjectOnGitApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
