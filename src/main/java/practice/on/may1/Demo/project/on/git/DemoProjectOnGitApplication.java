0c22100b177f4338b61ebac1b24f32048ca9db82
Manohar Reddy <manoharreddychiluka1516@gmail.com>
2024-05-21 17:15:30 -0500

    added

---
+++ src/main/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplication.java
@@ -0,0 +1,13 @@
+package practice.on.may1.Demo.project.on.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectOnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectOnGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/on/may1/Demo/project/on/git/DemoProjectOnGitApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.on.may1.Demo.project.on.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectOnGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
